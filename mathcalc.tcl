d2c9d13c88a8505bc8eefa50830f7699099c2815
Elfmagi <elfmagi@live.com>
2012-09-29 19:38:35 -0500

    Updated mathcalc.tcl to make "prettier"

--- mathcalc.tcl
+++ mathcalc.tcl
@@ -3,9 +3,11 @@
 # Math Calculator Add/Mult/Div/Subt
 # 20120925
 ######################################
+
 ################################
 #Main Code
 ################################
+
 if [winfo exists .msg] {destroy .msg1 .msg2 .msg3 .msg4}
 if [winfo exists .button1] {destroy .button1 .button2 .button3 .button4}
 if [winfo exists .entry1] {destroy .entry1 .entry2}
@@ -15,9 +17,11 @@ message .msg1 -width 15c -text "Enter your numbers here"
 # This is how we will enter our input
 entry .entry1 -textvar entry1Var
 entry .entry2 -textvar entry2Var
+
 ###############################
 # Buttons with commands
 ###############################
+
 button .button1 -command {.msg1 configure -text "$entry1Var + $entry2Var = [expr $entry1Var+$entry2Var]"} -text "Addition"
 button .button2 -command {.msg1 configure -text "$entry1Var * $entry2Var = [expr $entry1Var*$entry2Var]"} -text "Multiply"
 button .button3 -command {.msg1 configure -text "$entry1Var / $entry2Var = [expr $entry1Var/$entry2Var]"} -text "Division"
