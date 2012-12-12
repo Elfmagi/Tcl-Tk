22e0e5bb03f49e04df4c54097aa7f8ce996c34ed
Elfmagi <elfmagi@live.com>
2012-12-12 16:00:24 +0000

    Updated procedure for configuring buttons.

--- register.tcl
+++ register.tcl
@@ -5,11 +5,10 @@
 #####################################################
 #Procedure for Clean
 proc clean {} {
-  for {set n 0} {$n<=41} {incr n 1} {
+	for {set n 0} {$n<=41} {incr n 1} {
 		if [winfo exists .button$n] {destroy .button$n}
 	}
 if [winfo exists .mainMenu] {destroy .mainMenu}
-
 }
 
 #Procedure for mainMenuCommand
@@ -18,86 +17,33 @@ proc mainMenuCommand {buttonNumber} {
 		addItem 9999.99
 		return "WidowMaker"
 	} elseif {$buttonNumber==1} {
-		addItem
-		return ""
+		addItem 8.00
+		return "Cake"
 	} elseif {$buttonNumber==2} {
 		addItem 3.86
-		return ""
+		return "Hamburger"
 	} elseif {$buttonNumber==3} {
-		addItem
-		return ""
-	} elseif {$buttonNumber==4} {
-		addItem
-		return ""
-	} elseif {$buttonNumber==5} {
-		addItem
-		return ""
-	} elseif {$buttonNumber==6} {
-		addItem
-		return ""
-	} elseif {$buttonNumber==7} {
-		addItem
-		return ""
-	} elseif $buttonNumber==8} {
-		addItem
-		return ""
-	} elseif {$buttonNumber==9} {
-		addItem
-		return ""
-	} elseif {$buttonNumber==10} {
-		addItem
-		return ""
-	} elseif {$buttonNumber==11} {
-		addItem
-		return ""
-	} elseif {$buttonNumber==12} {
-		addItem
-		return ""
-	} elseif {$buttonNumber==13} {
-		addItem
-		return ""
-	} elseif {$buttonNumber==14} {
-		addItem
-		return ""
-	} elseif {$buttonNumber==15} {
-		addItem
-		return ""
-	} elseif {$buttonNumber==16} {
-		addItem
-		return ""
-	} elseif {$buttonNumber==17} {
-		addItem
-		return ""
-	} elseif {$buttonNumber==18} {
-		addItem
-		return ""
-	} elseif {$buttonNumber==19} {
-		addItem
-		return ""
-	} elseif {$buttonNumber==20} {
-		addItem
-		return ""
-	} elseif {$buttonNumber==21} {
-		addItem
-		return ""
-	} elseif {$buttonNumber==22} {
-		addItem
-		return ""
+		addItem 5.45
+		return "Sandwhich"
 	}
 }
+
+#addItem
+proc addItem {itemPrice} {
+
 #Run Clean
 clean
 
 #Create buttons
 frame .mainMenu
-for {set b 0} {$b<=41} {incr b 1} {
+for {set b 0} {$b<=3} {incr b 1} {
 	button .button$b -command {
 		set textv [mainMenuCommand $b]
 	}
 }
 
 #Command to create & place buttons
-for {set b 0} {$b<=41} {incr b 1} {
+for {set b 0} {$b<=3} {incr b 1} {
 	set col [expr $b%6]
 	set row [expr $b/6]
 	grid configure .button$b -in .mainMenu -column $col -row $row
